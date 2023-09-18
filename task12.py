63e56c52bec17745a1ebbcf6533b65174960dbf2
Stefan Strand <stefanboytchev@hotmail.com>
2023-09-18 16:37:51 +0200

    Saving before update

--- task12.py
+++ task12.py
@@ -1,2 +1,5 @@
-#  While loops from Free Code Camp
+#  for loops from Free Code Camp
 
+for letter in "Giraffe Academy":
+    print(letter)
+# The for loop in this case iterates over each letter in the string: "Giraffe Academy"
